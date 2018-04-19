d39d52c2b92ea1aef9fed63a9fbf34e407153db8
pavel-else <de.pavel.se@yandex.ru>
2018-04-19 23:34:56 +0300

    autoprefix э

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'), // Подключаем Gulp
     sass = require('gulp-sass'); // Подключаем Sass пакет
 var browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() { // Создаем таск "sass"
   return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
@@ -19,6 +20,16 @@ gulp.task('bs', function() {
  		},
   	})
 });
+
+ 
+gulp.task('autoprefixer', () =>
+    gulp.src('css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('css'))
+);
 
 
 gulp.task('default', ['watch']);
