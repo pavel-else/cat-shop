000de87c906d8ed8c161430e7fdff77caef9c71f
pavel-else <de.pavel.se@yandex.ru>
2018-04-22 21:24:47 +0300

    Исправлен баг со скроллингом на мобильных разрешениях

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@ var gulp = require('gulp'), // Подключаем Gulp
     sass = require('gulp-sass'); // Подключаем Sass пакет
 var browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function() { // Создаем таск "sass"
   return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
     .pipe(gulp.dest('css/')) // Выгружаем результата в папку css
-  });
+});
 
 gulp.task('watch', function() {
   gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
@@ -28,11 +29,10 @@ gulp.task('autoprefixer', () =>
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('css'))
+	.pipe(gulp.dest('css'))
 );
 
  
-let cleanCSS = require('gulp-clean-css');
 gulp.task('minify', () => {
   return gulp.src('css/style.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
