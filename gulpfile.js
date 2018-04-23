d593e7b24da1552374dd85c9a94d5236f08e898c
pavel-else <de.pavel.se@yandex.ru>
2018-04-23 23:19:22 +0300

    Рефакторинг э

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
-var gulp = require('gulp'), // Подключаем Gulp
-    sass = require('gulp-sass'); // Подключаем Sass пакет
+var gulp = require('gulp'); // Подключаем Gulp
+var sass = require('gulp-sass'); // Подключаем Sass пакет
 var browserSync = require('browser-sync');
-const autoprefixer = require('gulp-autoprefixer');
-let cleanCSS = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
 var autopolyfiller = require('gulp-autopolyfiller');
 
-gulp.task('sass', function() { // Создаем таск "sass"
+gulp.task('sass', () => { // Создаем таск "sass"
   return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
     .pipe(gulp.dest('css/')) // Выгружаем результата в папку css
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
+gulp.task('watch', () => {
+    gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
 });
 
-gulp.task('bs', function() {
+gulp.task('bs', () => {
   	browserSync({
  		server: {
  			baseDir: './'
  		},
   	})
 });
-
  
 gulp.task('autoprefixer', () =>
     gulp.src('css/style.css')
@@ -33,12 +32,11 @@ gulp.task('autoprefixer', () =>
         }))
 	.pipe(gulp.dest('css'))
 );
-
  
 gulp.task('minify', () => {
-  return gulp.src('css/style.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css'));
+    return gulp.src('css/style.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('css'));
 });
 
  
@@ -59,7 +57,7 @@ gulp.task('imagemin', () =>
 );
 
 
-gulp.task('polyfill', function () {
+gulp.task('polyfill', () => {
     return gulp.src('js/scripts.js')
         .pipe(autopolyfiller('result_polyfill_file.js'))
         .pipe(gulp.dest('js'));
